e2c556794c11f7ef1553078b950e705663e96ec3
tom <Jack@edureka.com>
2019-04-17 17:13:45 +0530

    adding testcases

--- src/test/java/com/demo/springboot/DTHServicesApplicationTests.java
+++ src/test/java/com/demo/springboot/DTHServicesApplicationTests.java
@@ -1,16 +1,60 @@
 package com.demo.springboot;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.springboot.model.Channel;
+import com.demo.springboot.model.User;
+import com.demo.springboot.service.DTHService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DTHServicesApplicationTests {
+	
+	@Autowired
+	private DTHService service;
+	
+	private String validUserId = "10021";
+	private String validUserName = "Rick Thomas";
+	private int validUserChannelCount = 7;
+	private int validUsersCount = 2;
+	private String invalidUserId = "ABC001";
+	
+	 @Before	
+	 public void beforeMethod(){
+	 }
+	  
+	@After 
+	public void afterMethod() {
+	} 
+	
+	 @Test
+	 public void testRetrieveUser() {
+		 User user = service.retrieveUser(validUserId);
 
-	@Test
-	public void contextLoads() {
-	}
+		 assertEquals("User Name Invalid", user.getName(),validUserName);
+	 }
+	 
+	 @Test
+	 public void testRetrieveAllUsers() {
+		 List<User> userList = service.retrieveAllUsers();
+		 assertTrue("Invalid User Count", (userList.size() == validUsersCount));
+	 }
+	 
+	 @Test
+	 public void testRetrieveChannels() {
+		 List<Channel> channelsList = service.retrieveChannels(validUserId);
+	    assertTrue("Invalid Channel Count", channelsList.size() == validUserChannelCount);
+	 }
 
 }
